6c1bc5c33d9260eadb46a25837f75a3d1c35958a
JasperNoorkoiv <33310229+JasperNoorkoiv@users.noreply.github.com>
2018-10-24 15:19:37 +0300

    korras
    ++

--- WeatherApp/WeatherApp/CustomAdapter.cs
+++ WeatherApp/WeatherApp/CustomAdapter.cs
@@ -38,17 +38,60 @@ namespace WeatherApp
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
             View view = convertView;
-
             if (view == null)
             {
                 view = context.LayoutInflater.Inflate(Resource.Layout.CustomRow, null);
             }
-            //DateTime date = Convert.ToDateTime(weathers[position].Date);
-            //string day = date.ToString("dddd").Substring(0, 3) + " " + date.ToString("HH:mm");
-            //view.FindViewById<TextView>(Resource.Id.SDate).Text = day;
-            //view.FindViewById<TextView>(Resource.Id.STemp).Text = weathers[position].Temperature;
-            view.FindViewById<ImageView>(Resource.Id.Layout1Image).SetImageResource(Resource.Drawable._01d);
 
+
+            ImageView Icon = view.FindViewById<ImageView>(Resource.Id.Layout1Image);
+
+            switch (weathers[position].ImageName)
+            {
+                case "01d":
+                case "01n":
+                    Icon.SetImageResource(Resource.Drawable._01d);
+                    break;
+                case "02d":
+                case "02n":
+                    Icon.SetImageResource(Resource.Drawable._02d);
+                    break;
+                case "03d":
+                case "03n":
+                    Icon.SetImageResource(Resource.Drawable._03d);
+                    break;
+                case "04d":
+                case "04n":
+                    Icon.SetImageResource(Resource.Drawable._04d);
+                    break;
+                case "09d":
+                case "09n":
+                    Icon.SetImageResource(Resource.Drawable._09d);
+                    break;
+                case "10d":
+                case "10n":
+                    Icon.SetImageResource(Resource.Drawable._10d);
+                    break;
+                case "11d":
+                case "11n":
+                    Icon.SetImageResource(Resource.Drawable._11d);
+                    break;
+                case "13d":
+                case "13n":
+                    Icon.SetImageResource(Resource.Drawable._13d);
+                    break;
+                case "50d":
+                case "50n":
+                    Icon.SetImageResource(Resource.Drawable._50d);
+                    break;
+                default:
+                    break;
+            }
+            
+            view.FindViewById<TextView>(Resource.Id.input).Text = weathers[0].City;
+            view.FindViewById<TextView>(Resource.Id.tempHigh).Text = (weathers[position].Temperature);
+            //view.FindViewById<TextView>(Resource.Id.humidity).Text = weathers[position].humidity;
+            view.FindViewById<TextView>(Resource.Id.date).Text = Convert.ToDateTime(weathers[position].Date).ToString("dddd");
             return view;
         }
     }
